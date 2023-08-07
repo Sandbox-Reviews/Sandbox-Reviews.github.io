58cd45a189209690b0357c57c8e9bb32ecdc1b60
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-07 18:06:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let params = new URLSearchParams(location.search);
 let user = params.get("user");
-let data = ''
-document.getElementById("json_data").innerHTML = `",${data},"alsojunk":"`
+let database = {"1":"2"}
+let realdata = `"schemaVersion": 1, "label": "${user}'s Sandbox Review Points", "message": "${database[user]}", "color": "white"`
+document.getElementById("json_data").innerHTML = `",${realdata},"alsojunk":"`
