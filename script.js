4674388f721d3c8a6dc25dab9f0ded398a419e9f
Little Monkey <106759534+PlaceReporter99@users.noreply.github.com>
2023-08-07 18:15:47 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let params = new URLSearchParams(location.search);
-let user = params.get("user");
-let database = {"1":"2"}
-let realdata = `"schemaVersion": 1, "label": "${user}'s Sandbox Review Points", "message": "${database[user]}", "color": "white"`
-document.getElementById("json_data").innerHTML = `",${realdata},"alsojunk":"`
